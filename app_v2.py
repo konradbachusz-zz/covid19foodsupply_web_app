8bba03d3846dc50a529078626e622093abc82e4d
Konrad <37801868+konradbachusz@users.noreply.github.com>
2020-05-10 18:33:26 +0100

    Updated the web app and created the geojson files

--- app_v2.py
+++ app_v2.py
@@ -5,6 +5,13 @@ from PIL import Image
 import streamlit as st
 from bokeh.plotting import figure
 import pydeck as pdk
+import math
+import geojson
+import plotly.express as px
+
+#Load UK Map
+with open('UK.geojson') as f:
+    geojson_map = geojson.load(f)
 
 #Logo
 image=Image.open("logo-new-min.png")
@@ -28,7 +35,7 @@ regions = st.sidebar.multiselect("Select regions",("All","Scotland","Northern Ir
 
 #Dataframe
 df=pd.read_csv("dummy_data.csv")
-#st.markdown(df.columns)
+
 
 #scenario filtering
 if scenario=="Full lockdown":
@@ -45,107 +52,67 @@ if len(regions)>0:
     else:
         df=df
         df=df[df.region.isin(regions)]
-#st.dataframe(df)
+
 
 #Risk Factor
+#scenario criteria
 if scenario=="Full lockdown":
-    df['Risk Factor']=df.intercept+df.resilience_coefficient*resilience+df.recovery_coefficient*recovery+df.food_safety_stock_coefficient*food_safety_stock-10
+    df['Risk Factor']=df.intercept+df.resilience_coefficient*resilience+df.recovery_coefficient*recovery+df.food_safety_stock_coefficient*food_safety_stock
 elif scenario=="Partial lockdown":
     df['Risk Factor']=df.intercept+df.resilience_coefficient*resilience+df.recovery_coefficient*recovery+df.food_safety_stock_coefficient*food_safety_stock+20
 elif scenario=="Full re-opening":
     df['Risk Factor']=df.intercept+df.resilience_coefficient*resilience+df.recovery_coefficient*recovery+df.food_safety_stock_coefficient*food_safety_stock+30
 
+#interventions criteria
+if "Closed Airports" in interventions:
+    df['Risk Factor']=df['Risk Factor']-5
+elif "Closed Ports" in interventions:
+    df['Risk Factor']=df['Risk Factor']-5
+elif "Add another route to distribution channel" in interventions:
+    df['Risk Factor']=df['Risk Factor']-5
+elif "Add another route to the processing centre" in interventions:
+    df['Risk Factor']=df['Risk Factor']-5
+
 st.markdown("Average Risk Factor: "+str(round(df['Risk Factor'].mean(),2)))
+st.markdown("Number of people impacted: "+str(round(df['Risk Factor'].mean()*98,0)))
+st.markdown("Expected monetary impact: £"+str(round(df['Risk Factor'].mean()*98*9,0)))
 
 #Bar chart
 df['Resilience']=resilience
+st.title("Risk Factor by region")
 st.bar_chart(df.groupby('region')[['Risk Factor','Resilience']].mean(),height=550)
 
-
-
-#Infections Chart
-if scenario=="No" and airports_closed=="No":
-    x = np.linspace(0, 3, 100)
-    y = np.exp(x)
-    p = figure(title="Food Shortage per household in 1000s kcal",x_axis_label="Years since 1st infection",y_axis_label="Food Shortage per household in 1000s kcal")
-    p.line(x, y, legend="Trend", line_width=2)
-    st.bokeh_chart(p, use_container_width=True)
-elif scenario=="Yes" and airports_closed=="No": 
-    x = np.linspace(0, 3, 100)
-    y = x*2
-    p = figure(title="Food Shortage per household in 1000s kcal",x_axis_label="Years since 1st infection",y_axis_label="Food Shortage per household in 1000s kcal")
-    p.line(x, y, legend="Trend", line_width=2)
-    st.bokeh_chart(p, use_container_width=True)
-
-elif scenario=="Yes" and airports_closed=="Yes":
-    x = np.linspace(0, 3, 100)
-    y = -np.exp(-x)
-    p = figure(title="Food Shortage per household in 1000s kcal",x_axis_label="Years since 1st infection",y_axis_label="Food Shortage per household in 1000s kcal")
-    p.line(x, y, legend="Trend", line_width=2)
-    st.bokeh_chart(p, use_container_width=True)
-#test map
+#Plotly choropleth
+fig = px.choropleth(df, geojson=geojson_map , color="Risk Factor",
+                    locations="region", featureidkey="properties.EER13NM",
+                    projection="mercator"
+                   )
+fig.update_geos(fitbounds="locations", visible=False)
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+st.plotly_chart(fig)
 
 #Map
-st.markdown("\nFood Banks and Supermarkets")
-cities=pd.read_csv("cities.csv")
-#st.map(cities[['lat','lon']]) #simple map
+st.title("\nFood Banks")
+food_banks=pd.read_csv("foodbank_zw.csv")
+food_banks['lat']=food_banks["Latitude"]
+food_banks['lon']=food_banks["Longitude"]
+food_banks=food_banks[['food_bank','lat','lon']].dropna()
 
-midpoint = (np.average(cities["lat"]), np.average(cities["lon"]))
+
+midpoint = (np.average(food_banks["lat"]), np.average(food_banks["lon"]))
 
 st.deck_gl_chart(
             viewport={
                 "latitude": midpoint[0],
                 "longitude":  midpoint[1],
-                "zoom": 4
+                "zoom": 5
             },
             layers=[{
                 "type": "ScatterplotLayer",
-                "data": cities,
-                "radiusScale": 250,
-   "radiusMinPixels": 5
+                "data": food_banks,
+                "radiusScale": 100,
+            "radiusMinPixels": 2,
+
                 
             }]
         )
-
-
-#Map2
-st.markdown("Food Shortage")
-df = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [51.5074, 0.1278],
-    columns=["lat", "lon"])
-
-st.pydeck_chart(pdk.Deck(
-     map_style="mapbox://styles/mapbox/light-v9",
-     initial_view_state=pdk.ViewState(
-         latitude=51.5074,
-         longitude=0.1278,
-         zoom=11,
-         pitch=50,
-     ),
-     layers=[
-         pdk.Layer(
-            "HexagonLayer",
-            data=df,
-            get_position="[lon, lat]",
-            auto_highlight=True,
-            radius=200,
-            elevation_scale=4,
-            elevation_range=[0, 1000],
-            pickable=True,
-            extruded=True,
-            coverage=1
-         ),
-         pdk.Layer(
-             "ScatterplotLayer",
-             data=df,
-             get_position="[lon, lat]",
-             get_color="[200, 30, 0, 160]",
-             get_radius=200,
-         ),
-     ],
- ))
-
-
-
-
-
